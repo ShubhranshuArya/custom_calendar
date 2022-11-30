6d10f7f8a6b8512bf08b869399b297a0a62d7607
BotCoder <77967955+ShubhranshuArya@users.noreply.github.com>
2022-11-30 15:26:03 +0530

    updated

--- lib/widgets/calendar_widget.dart
+++ lib/widgets/calendar_widget.dart
@@ -21,61 +21,63 @@ class _CalendarWidgetState extends State<CalendarWidget> {
     return Container(
       width: double.infinity,
       padding: EdgeInsets.symmetric(horizontal: 8.w),
-      child: TableCalendar(
-        firstDay: DateTime.utc(2022, 1, 31),
-        lastDay: DateTime.utc(2023, 2, 28),
-        focusedDay: calendarController.toggleDate.value,
-        startingDayOfWeek: StartingDayOfWeek.sunday,
-        rowHeight: 42.h,
-        calendarFormat: CalendarFormat.month,
-        availableGestures: AvailableGestures.horizontalSwipe,
-        selectedDayPredicate: (day) {
-          return isSameDay(day, calendarController.toggleDate.value);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          setState(() {
-            calendarController.toggleDate.value = selectedDay;
-          });
-        },
-        headerStyle: HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-          titleTextStyle: robotoBlack18w6,
-          leftChevronPadding: EdgeInsets.only(left: 56.w),
-          rightChevronPadding: EdgeInsets.only(right: 56.w),
-          leftChevronIcon: Icon(
-            Icons.arrow_left_rounded,
-            color: blackSecondary,
-            size: 40.h,
+      child: Obx(() {
+        return TableCalendar(
+          firstDay: DateTime.utc(2022, 1, 31),
+          lastDay: DateTime.utc(2023, 2, 28),
+          focusedDay: calendarController.toggleDate.value,
+          startingDayOfWeek: StartingDayOfWeek.sunday,
+          rowHeight: 42.h,
+          calendarFormat: CalendarFormat.month,
+          availableGestures: AvailableGestures.horizontalSwipe,
+          selectedDayPredicate: (day) {
+            return isSameDay(day, calendarController.toggleDate.value);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              calendarController.toggleDate.value = selectedDay;
+            });
+          },
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextStyle: robotoBlack18w6,
+            leftChevronPadding: EdgeInsets.only(left: 56.w),
+            rightChevronPadding: EdgeInsets.only(right: 56.w),
+            leftChevronIcon: Icon(
+              Icons.arrow_left_rounded,
+              color: blackSecondary,
+              size: 40.h,
+            ),
+            rightChevronIcon: Icon(
+              Icons.arrow_right_rounded,
+              color: blackSecondary,
+              size: 40.h,
+            ),
           ),
-          rightChevronIcon: Icon(
-            Icons.arrow_right_rounded,
-            color: blackSecondary,
-            size: 40.h,
+          daysOfWeekStyle: DaysOfWeekStyle(
+            weekdayStyle: robotoBlack15w4,
+            weekendStyle: robotoBlack15w4,
           ),
-        ),
-        daysOfWeekStyle: DaysOfWeekStyle(
-          weekdayStyle: robotoBlack15w4,
-          weekendStyle: robotoBlack15w4,
-        ),
-        calendarStyle: CalendarStyle(
-          outsideDaysVisible: true,
-          isTodayHighlighted: true,
-          todayTextStyle: robotobluePri15w4,
-          todayDecoration: const BoxDecoration(
-            color: Colors.transparent,
-            shape: BoxShape.circle,
+          calendarStyle: CalendarStyle(
+            outsideDaysVisible: true,
+            isTodayHighlighted: true,
+            todayTextStyle: robotobluePri15w4,
+            todayDecoration: const BoxDecoration(
+              color: Colors.transparent,
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: const BoxDecoration(
+              color: bluePrimary,
+              shape: BoxShape.circle,
+            ),
+            selectedTextStyle: robotoWhite15w4,
+            defaultTextStyle: robotoBlack15w4,
+            weekendTextStyle: robotoBlack15w4,
+            outsideTextStyle: robotoWhiteSec15w4,
           ),
-          selectedDecoration: const BoxDecoration(
-            color: bluePrimary,
-            shape: BoxShape.circle,
-          ),
-          selectedTextStyle: robotoWhite15w4,
-          defaultTextStyle: robotoBlack15w4,
-          weekendTextStyle: robotoBlack15w4,
-          outsideTextStyle: robotoWhiteSec15w4,
-        ),
-      ),
+        );
+      }),
     );
   }
 }
